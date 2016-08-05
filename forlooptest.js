b84a2ecc3d3ce53db0b50bf684ae3958cf00f816
Muniswamy <vijaymuniswamy9@gmail.com>
2016-08-04 20:33:54 -0400

    js snippets fo runderstanding more java concepts

---
+++ forlooptest.js
@@ -0,0 +1,29 @@
+
+var arr = ['vijay','vinay', ,'nanna'];
+
+exec(function(err) {
+    if (err) {
+        console.log(err+'eroor');
+    }
+    return;
+});
+
+function exec(callback) {
+    for (var i = 0; i < arr.length; i++) {
+        RPC(arr[i],function(err) {
+            if (err) {
+                return callback(err);
+            }
+            callback();
+        })
+    }
+}
+
+
+function RPC(a,cb) {
+    if(a!=null){
+        console.log(a+'dssdsd');
+        return cb;
+    }
+    return cb('error');
+}
